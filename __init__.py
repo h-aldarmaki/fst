13bd514c21a638969e891cba9c7826d1dbc1e499
Hanan Aldarmaki <hanan.aldarmaki@gmail.com>
2021-07-13 13:07:16 +0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+Finite state transducer support, including graphical display & demo,
+and interface code for AT&T fsmtools.
+
+This is a modified version of the nltk_contrib module, which can be 
+accessed here:
+
+  https://github.com/nltk/nltk_contrib.git
+
+The modifications are only to make the code work with python3. 
+
+"""
